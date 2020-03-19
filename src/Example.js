6cc53a2037dcabda84ba6e9f4674c943be7c0656
Harry Wang <harryjwang@gmail.com>
2020-03-18 21:05:48 -0400

    finished list and keys

--- src/Example.js
+++ src/Example.js
@@ -23,10 +23,68 @@ const element = (
   </h1>
 );
 
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    return (
+    <ul>
+        {numbers.map((number) =>
+            <ListItem key={number.toString()}
+                value={number} />
+        )}
+    </ul>
+    );
+}
+
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {
+                props.posts.map((post) =>
+                    <li key={post.id}>
+                        {post.title}
+                    </li>
+                )
+            }
+        </ul>
+    );
+
+    const content = props.posts.map((post)=>
+    <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+    </div>
+);
+
+    return (
+        <div>
+            {sidebar}
+            <hr/>
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+  {id: 1, title: 'Post 1: Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Post 2: Installation', content: 'You can install React from npm.'}
+];
+
+
 class Example extends React.Component{
     render(){
         return (
-            element
+
+            <div>
+                {element}
+                <Blog posts={posts} />
+                <NumberList numbers={[1, 3, 5]}/>
+            </div>
+
         );
     }
 }
