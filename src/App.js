b51d8062759ff2c1fcbb067acbfdf179d6e7f3b8
Harry Wang <harryjwang@gmail.com>
2020-03-19 14:00:54 -0400

    added thinking in react example

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Toggle from './Toggle';
 import LoginControl from './LoginControl';
 import NameForm from './NameForm'
 import Calculator from './Calculator'
+import FilterableProductTable from './ProductSearch'
+
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 function App() {
     return (
@@ -18,6 +29,7 @@ function App() {
             <LoginControl />
             <NameForm />
             <Calculator />
+            <FilterableProductTable products={PRODUCTS} />
         </div>
 
     );
